0d89a33264010107d7287b8c0af74e8bc45d8b79
Alisson10101010 <alisson.teixeira@iesb.edu.br>
2024-09-23 21:55:10 -0300

    Atividade 05

--- nome_atividade05/src/app/components/BarraNavegacao.js
+++ nome_atividade05/src/app/components/BarraNavegacao.js
@@ -11,16 +11,16 @@ export default function BarraNavegacao() {
 
         <Navbar expand="lg" bg="dark" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="/">Movies</Navbar.Brand>
+                <Navbar.Brand href="/">Series</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
 
                         {/* Menu Dropdown */}
                         <NavDropdown title="Filmes" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/filmes">Filmes Populares</NavDropdown.Item>
+                            <NavDropdown.Item href="/filmes">Series Populares</NavDropdown.Item>
                             <NavDropdown.Item href="/filmes/top">Melhores Avaliados</NavDropdown.Item>
-                            <NavDropdown.Item href="/filmes/cartaz">Em Cartaz</NavDropdown.Item>
+                            <NavDropdown.Item href="/filmes/cartaz">Cartaz</NavDropdown.Item>
                             <NavDropdown.Item href="/filmes/lancamentos">Nos Cinemas</NavDropdown.Item>
                         </NavDropdown>
 
